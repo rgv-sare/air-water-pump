c45a217664e071138aa3d0ac23016f135a67bf27
emmanuelramirez175 <132167121+emmanuelramirez175@users.noreply.github.com>
2023-04-29 15:06:49 -0500

    Create air-water-pump-2.0.ino
    This code will have the air pump running 30 minutes on and 30 minutes off for the 8 hours that the water pump will be off. Then the water pump will turn on for 1 min. The code then resets

---
+++ air-water-pump-2.0.ino
@@ -0,0 +1,30 @@
+const int relayPin1 = 7; // air pump
+const int relayPin2 = 8; // water pump
+unsigned long waterPumpStartTime = 0;
+const unsigned long waterPumpOffTime = 28800000; // 8 hours
+
+void setup()
+{
+  pinMode(relayPin1, OUTPUT); // set the relay pin as an output - air pump
+  pinMode(relayPin2, OUTPUT); // set the relay pin as an output - water pump
+}
+
+void loop()
+{
+  waterPumpStartTime = 0;
+  digitalWrite(relayPin2, LOW);
+  waterPumpStartTime = millis(); // start time
+
+  while(millis() - waterPumpStartTime < waterPumpOffTime)
+  {
+    // air pump
+    digitalWrite(relayPin1, HIGH);
+    delay(1800000); // 30 mins on
+    digitalWrite(relayPin1, LOW);
+    delay(1800000); // 30 mins off
+  }
+
+  digitalWrite(relayPin2, HIGH);
+  delay(60000); // 1 min on
+  digitalWrite(relayPin2, LOW);
+}
